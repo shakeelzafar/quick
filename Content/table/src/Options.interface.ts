7829015c6a76f932080576bc1fed9dbd654a382a
shakeelzafar <bsef12m511@pucit.edu.pk>
2017-10-28 16:29:37 +0500

    changes
    are here

---
+++ Content/table/src/Options.interface.ts
@@ -0,0 +1,7 @@
+/** Plugin Options */
+interface Options {
+    columnSelector: string,
+    sort: boolean,
+    search: boolean
+}
+
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="dreport.MvcApplication" Language="C#" %>
